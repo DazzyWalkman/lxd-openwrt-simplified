62ea0b808ce79f53e18f6ab6d0daea1253e7dc08
Dazzy Walkman <dazzy@debian9xfce4.dazzy.home>
2021-10-24 21:07:06 +0800

    cosmetic: improve readability
    shell format with flag -ci,-s
    and
    replace test with [].

--- build_cust.sh
+++ build_cust.sh
@@ -2,29 +2,29 @@
 set -e
 export LC_ALL=C
 super=fakeroot
-ver=$(basename "$1"|cut -d'-' -f 2)
-dist=$(basename "$1"|cut -d'-' -f 1)
+ver=$(basename "$1" | cut -d'-' -f 2)
+dist=$(basename "$1" | cut -d'-' -f 1)
 type=$2
 usage() {
 	echo "Usage: $0 <rootfs tarball> <lxd|plain> "
 	exit 1
 }
-if [ "$type" != plain ] && [ "$type" != lxd ] ; then
-usage
-exit 1
+if [ "$type" != plain ] && [ "$type" != lxd ]; then
+	usage
+	exit 1
 fi
-revision=$(basename "$1"|cut -d'-' -f 3-4)
+revision=$(basename "$1" | cut -d'-' -f 3-4)
 rootfs="$1"
-arch_lxd="$(tar xf "$rootfs" ./etc/openwrt_release -O|grep DISTRIB_ARCH|sed -e "s/.*='\(.*\)'/\1/")"
+arch_lxd="$(tar xf "$rootfs" ./etc/openwrt_release -O | grep DISTRIB_ARCH | sed -e "s/.*='\(.*\)'/\1/")"
 if [ ! "$arch_lxd" ]; then
-echo "Unknown CPU arch. Possible an invalid OpenWRT rootfs tarball. Failed."
-exit 1
-fi 
+	echo "Unknown CPU arch. Possible an invalid OpenWRT rootfs tarball. Failed."
+	exit 1
+fi
 tarball=bin/${dist}-${ver}-${revision}-${arch_lxd}-${type}.tar.gz
 metadata=bin/metadata.yaml
 build_tarball() {
 	local opts=""
-	if test "${type}" = lxd; then
+	if [ "${type}" = lxd ]; then
 		opts="$opts -m $metadata"
 	fi
 	local cmd=""
@@ -42,14 +42,16 @@ build_tarball() {
 				exit 1
 				;;
 		esac
-	fi	
+	fi
 	$cmd "$rootfs" $opts -o "$tarball" --disable-services="sysfixtime sysntpd led urngd"
 }
 build_metadata() {
 	local desc=""
-	desc="$(tar xf "$rootfs" ./etc/openwrt_release -O|grep DISTRIB_DESCRIPTION|sed -e "s/.*='\(.*\)'/\1/")"
-	test -e bin || mkdir bin
-	cat > $metadata <<EOF
+	desc="$(tar xf "$rootfs" ./etc/openwrt_release -O | grep DISTRIB_DESCRIPTION | sed -e "s/.*='\(.*\)'/\1/")"
+	if [ ! -d bin ]; then
+		mkdir bin
+	fi
+	cat >$metadata <<EOF
 architecture: "$arch_lxd"
 creation_date: $(date +%s)
 properties:
--- scripts/build_rootfs_cs.sh
+++ scripts/build_rootfs_cs.sh
@@ -13,20 +13,30 @@ temp=$(getopt -o "d:o:f:m:" -l "disable-services:,output:,files:,metadata:,help"
 eval set -- "$temp"
 while true; do
 	case "$1" in
-		-d|--disable-services)
-            services="$2"; shift 2;;
-		-o|--output)
-			dst_file="$2"; shift 2;;
-		-f|--files)
-			files="$2"; shift 2;;
-		-m|--metadata)
+		-d | --disable-services)
+			services="$2"
+			shift 2
+			;;
+		-o | --output)
+			dst_file="$2"
+			shift 2
+			;;
+		-f | --files)
+			files="$2"
+			shift 2
+			;;
+		-m | --metadata)
 			metadata=$(basename "$2")
 			metadata_dir=$(dirname "$2")
-			shift 2;;
+			shift 2
+			;;
 		--help)
-			usage;;
+			usage
+			;;
 		--)
-			shift; break;;
+			shift
+			break
+			;;
 	esac
 done
 if [ $# -ne 1 ]; then
@@ -34,7 +44,7 @@ if [ $# -ne 1 ]; then
 fi
 src_tar=$1
 dir=$(mktemp -d)
-if [ ! -d "$dir" ]; then 
+if [ ! -d "$dir" ]; then
 	echo "Failed to make temp dir. Abort."
 	exit 1
 fi
@@ -42,69 +52,72 @@ files_dir=files/
 instroot="$dir"/rootfs
 unpack() {
 	mkdir -p "$instroot"
-	 (cd "$instroot" && tar -xz) < "$src_tar"
+	(cd "$instroot" && tar -xz) <"$src_tar"
 }
 pack() {
 	echo Pack rootfs
-	if test -n "$metadata"; then
-	local TARGET_DIR="$dir"
+	if [ -n "$metadata" ]; then
+		local TARGET_DIR="$dir"
 	else
-	local TARGET_DIR="$dir"/rootfs
+		local TARGET_DIR="$dir"/rootfs
 	fi
-	tar -cp --sort=name -C "$TARGET_DIR" . | gzip -9n > "$dst_file"
+	tar -cp --sort=name -C "$TARGET_DIR" . | gzip -9n >"$dst_file"
 }
 disable_root() {
 	sed -i -e 's/^root::/root:*:/' "$instroot"/etc/shadow
 }
 add_file() {
-    file=$1
-    src_dir=$2
-    dst_dir=$3
-    src=$src_dir/$file
-    dst=$dst_dir/$file
-    if test -d "$src"; then
-	test -d "$dst" || mkdir -p "$dst"
-    elif test -f "$src"; then
-	cp "$src" "$dst"
-	foo=$(dirname "$file")
-	if [ "$foo" = "./etc/init.d" ]; then
-	    echo Enabling "$file"
-	    set +e
-	    env IPKG_INSTROOT="$instroot" sh "$instroot"/etc/rc.common "$dst" enable
-	    set -e
+	file=$1
+	src_dir=$2
+	dst_dir=$3
+	src=$src_dir/$file
+	dst=$dst_dir/$file
+	if [ -d "$src" ]; then
+		if [ ! -d "$dst" ]; then
+			mkdir -p "$dst"
+		fi
+	elif [ -f "$src" ]; then
+		cp "$src" "$dst"
+		foo=$(dirname "$file")
+		if [ "$foo" = "./etc/init.d" ]; then
+			echo Enabling "$file"
+			set +e
+			env IPKG_INSTROOT="$instroot" sh "$instroot"/etc/rc.common "$dst" enable
+			set -e
+		fi
 	fi
-    fi
 }
 add_files() {
 	src_dir=$1
 	dst_dir=$2
-	for f in $(cd "$src_dir" && find . ); do
+	for f in $(cd "$src_dir" && find .); do
 		add_file "$f" "$src_dir" "$dst_dir"
 	done
 }
 disable_services() {
-    local services="$1"
-    for service in $services; do
-	local init_script="$instroot"/etc/init.d/"$service"
-	if [ -x "$init_script" ]; then 
-        echo Disabling "$service"
-        env IPKG_INSTROOT="$instroot" sh "$instroot"/etc/rc.common "$init_script" disable
-		else echo "$service" not found. Skip.
+	local services="$1"
+	for service in $services; do
+		local init_script="$instroot"/etc/init.d/"$service"
+		if [ -x "$init_script" ]; then
+			echo Disabling "$service"
+			env IPKG_INSTROOT="$instroot" sh "$instroot"/etc/rc.common "$init_script" disable
+		else
+			echo "$service" not found. Skip.
 		fi
-    done
+	done
 }
 clean_up() {
 	rm -rf "$dir"
 }
 unpack
 disable_root
-if test -n "$metadata"; then
+if [ -n "$metadata" ]; then
 	add_file "$metadata" "$metadata_dir" "$dir"
 fi
 add_files templates/ "$dir"/templates/
 disable_services "$services"
 add_files $files_dir "$instroot"
-if test -n "$files"; then
+if [ -n "$files" ]; then
 	add_files "$files" "$instroot"
 fi
 pack

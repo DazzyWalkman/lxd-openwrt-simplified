ffd6cff295e0f515f83cda28519bcdb4df99656f
DazzyWalkman <20811819+DazzyWalkman@users.noreply.github.com>
2022-12-23 21:11:12 +0800

    Construct the output filename with elements from the openwrt_release file
    This can help make output filename more consistent.
    Also group those greps together to eliminate unnecessary extractions of the same file.

--- build_cust.sh
+++ build_cust.sh
@@ -2,22 +2,39 @@
 set -e
 export LC_ALL=C
 super=fakeroot
-ver=$(basename "$1" | cut -d'-' -f 2)
-dist=$(basename "$1" | cut -d'-' -f 1)
-type=$2
+rootfs="$1"
+if [ ! -s "$rootfs" ]; then
+	echo "Invalid file. Abort."
+	exit 1
+fi
 usage() {
 	echo "Usage: $0 <rootfs tarball> <lxd|plain> "
 	exit 1
 }
+type=$2
 if [ "$type" != plain ] && [ "$type" != lxd ]; then
 	usage
-	exit 1
 fi
-revision=$(basename "$1" | cut -d'-' -f 3-4)
-rootfs="$1"
-arch_lxd="$(tar xf "$rootfs" ./etc/openwrt_release -O | grep DISTRIB_ARCH | sed -e "s/.*='\(.*\)'/\1/")"
-if [ ! "$arch_lxd" ]; then
-	echo "Unknown CPU arch. Possible an invalid OpenWrt rootfs tarball. Failed."
+dir=$(mktemp -d)
+if [ -d "$dir" ]; then
+	if tar xf "$rootfs" ./etc/openwrt_release -O >"$dir"/openwrt_release; then
+		ver="$(grep DISTRIB_RELEASE "$dir"/openwrt_release | sed -e "s/.*='\(.*\)'/\1/")"
+		dist="$(grep DISTRIB_ID "$dir"/openwrt_release | sed -e "s/.*='\(.*\)'/\1/")"
+		revision="$(grep DISTRIB_REVISION "$dir"/openwrt_release | sed -e "s/.*='\(.*\)'/\1/")"
+		arch_lxd="$(grep DISTRIB_ARCH "$dir"/openwrt_release | sed -e "s/.*='\(.*\)'/\1/")"
+		desc="$(grep DISTRIB_DESCRIPTION "$dir"/openwrt_release | sed -e "s/.*='\(.*\)'/\1/")"
+		rm -rf "$dir"
+		if [ ! "$arch_lxd" ] || [ ! "$dist" ] || [ ! "$ver" ] || [ ! "$revision" ]; then
+			echo "Possible an invalid OpenWrt rootfs tarball. Failed."
+			exit 1
+		fi
+	else
+		echo "Invalid tarball. Abort."
+		rm -rf "$dir"
+		exit 1
+	fi
+else
+	echo "Failed to make temp dir. Abort."
 	exit 1
 fi
 tarball=bin/${dist}-${ver}-${revision}-${arch_lxd}-${type}.tar.gz
@@ -46,11 +63,7 @@ build_tarball() {
 	$cmd "$rootfs" $opts -o "$tarball" --disable-services="sysfixtime sysntpd led urngd"
 }
 build_metadata() {
-	local desc=""
-	desc="$(tar xf "$rootfs" ./etc/openwrt_release -O | grep DISTRIB_DESCRIPTION | sed -e "s/.*='\(.*\)'/\1/")"
-	if [ ! -d bin ]; then
-		mkdir bin
-	fi
+	mkdir -p bin
 	cat >$metadata <<EOF
 architecture: "$arch_lxd"
 creation_date: $(date +%s)
